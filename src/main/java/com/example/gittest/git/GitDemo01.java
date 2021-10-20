1ce68b25ac56697ec53b019bc628eff86460df42
mengshuo <mengshuo@123.com>
2021-10-20 13:58:15 +0800

    second commit

--- src/main/java/com/example/gittest/git/GitDemo01.java
+++ src/main/java/com/example/gittest/git/GitDemo01.java
@@ -3,5 +3,6 @@ package com.example.gittest.git;
 public class GitDemo01 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git333");
     }
 }
