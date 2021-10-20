e1f32cadc08489f66141a62ce16e8e14bea325e2
mengshuo <mengshuo@123.com>
2021-10-20 15:33:12 +0800

    分支ms commit

--- src/main/java/com/example/gittest/git/GitDemo01.java
+++ src/main/java/com/example/gittest/git/GitDemo01.java
@@ -10,5 +10,6 @@ public class GitDemo01 {
         System.out.println("hello hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("分支ms test");
     }
 }
